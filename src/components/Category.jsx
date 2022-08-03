dc213c57fbd250847d73db7e347cd1a8a42f2123
zer0-byte0 <k.fariasoha@gmail.com>
2022-08-03 16:17:01 +0600

    category added

---
+++ src/components/Category.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  font-size: 2rem;
+`;
+
+const Category = () => {
+  return <Container>All Categories</Container>;
+};
+
+export default Category;
